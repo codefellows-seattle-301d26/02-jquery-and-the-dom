86ed9603be1ed0bad050760b3cff312ab4d92ec4
Greg Nordeng <nordcoder@outlook.com>
2017-12-06 10:40:16 -0800

    jquery to fill out tempate with values

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -13,21 +13,26 @@ function Article (rawDataObj) {
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorURL;
   this.publishedOn = rawDataObj.publishedOn;
-  this.body = rawDataObj.body
+  this.body = rawDataObj.body;
+  articles.push(this)
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
 }
 
 Article.prototype.toHtml = function() {
-  // COMMENT: What is the benefit of cloning the article? (see the jQuery docs) 
+  // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
   // PUT YOUR RESPONSE HERE
   //Since we will be displaying many articles, we can clone a prototype and repeat it to render many versions of th article.//
 
-  let $newArticle = $('article.template').clone();
+  let $newArticle = $('article.template').clone().removeClass('template');
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
   if (!this.publishedOn) $newArticle.addClass('draft');
-  $newArticle.attr('data-category', this.category);
+  $newArticle.data('title', this.title);
+  $newArticle.data('author', this.author);
+  $newArticle.data('authorURL', this.authorURL);
+  $newArticle.data('publishedOn', this.publishedOn);
+  $newArticle.data('articleTitle', this.articleTitle);
 
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
