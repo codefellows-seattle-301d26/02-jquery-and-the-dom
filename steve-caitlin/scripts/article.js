642f4f947fa876339d800c2155f23a6555a58416
Steven Carpenter <stevegcarpenter@gmail.com>
2017-12-06 09:47:33 -0800

    Added some quick propertie assignments in the Article() constructor

--- steve-caitlin/scripts/article.js
+++ steve-caitlin/scripts/article.js
@@ -8,6 +8,12 @@ let articles = [];
 function Article (rawDataObj) {
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
+  this.title = title;
+  this.category = category;
+  this.author = author;
+  this.authorUrl = authorUrl;
+  this.publishedOn = publishedOn;
+  this.body = body;
 }
 
 Article.prototype.toHtml = function() {
