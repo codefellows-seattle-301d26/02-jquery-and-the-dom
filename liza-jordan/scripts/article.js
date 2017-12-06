fff39e9c28e84c83f675192b0394be8f791eb351
Jordan <JordanLesaca@Gmail.Com>
2017-12-06 12:09:27 -0800

    newArticle

--- liza-jordan/scripts/article.js
+++ liza-jordan/scripts/article.js
@@ -12,8 +12,7 @@ function Article (rawDataObj) {
   this.authorUrl = rawDataObj.authorUrl;
   this.publishedOn = rawDataObj.publishedOn;
   this.body = rawDataObj.body;
-  articles.push(this);
-  console.log('hi', articles);
+  // console.log('hi', articles);
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
 }
@@ -24,12 +23,18 @@ Article.prototype.toHtml = function() {
   // The .clone() method performs an exact copy of the set of matched elements. It will copy the matched elements and their descendant elements and text nodes. It's a convenient way to duplicate elements on a page.
 
   let $newArticle = $('article.template').clone();
-  
+  $newArticle.removeClass('template')
+    
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
-  // $('draft').add('author', 'authorUrl', 'title', 'body', 'publishedOn');
+  $('draft').add('author', 'authorUrl', 'title', 'body', 'publishedOn');
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('address').html(this.author);
+  $newArticle.find('time').html(this.publishedOn);
+  $newArticle.find('a').html(this.authorUrl);
+  $newArticle.find('.article-body').html(this.body);
 
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
