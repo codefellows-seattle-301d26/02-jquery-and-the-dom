c1f77193203ca6ac6990a541d12f906414e6dab4
CAITLIN SWEENEY <CAITLINSWEENEYLMT@GMAIL.COM>
2017-12-06 11:03:26 -0800

    moved hamburger menu

--- steve-caitlin/scripts/article.js
+++ steve-caitlin/scripts/article.js
@@ -44,10 +44,8 @@ Article.prototype.toHtml = function() {
 
 $(document).ready(function(){
   $('.main-nav').hide(); 
-  console.log('afsdlfkjalkfjladkfjalskjdfl');
   $('.icon-menu').click(function(){
     $('.main-nav').slideDown();
-    console.log('worked');
   });
 });
 
